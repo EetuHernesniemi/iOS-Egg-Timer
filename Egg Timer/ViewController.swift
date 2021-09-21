d9387903d8bac67ca7b50f1978b0c6c091bd94ad
Eetu Hernesniemi <eetu.hernesniemi@hotmail.com>
2021-09-21 10:58:17 +0300

    initial new xcode project files

---
+++ Egg Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg Timer
+//
+//  Created by Eetu Hernesniemi on 21.9.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
