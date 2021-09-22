a687ba6c93b31fc629f4005ca606a3e11e60c7e7
Eetu Hernesniemi <eetu.hernesniemi@hotmail.com>
2021-09-22 11:33:23 +0300

    egg timer value confirmed by print on click.

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let eggTimes : [String: Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness : String = sender.currentTitle!//Make sure egg buttons have correct title values.
+        print(hardness)
+        print(eggTimes[hardness]!)
+    }
 }
 
